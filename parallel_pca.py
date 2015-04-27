02ca12ae45bff5cbd8eb26947be973431d3933ea
Marcus Schorow <mschorow@college.harvard.edu>
2015-04-27 18:01:05 -0400

    rawr

--- parallel_pca.py
+++ parallel_pca.py
@@ -1,4 +1,4 @@
-from mrjob.job import MRJob
+# from mrjob.job import MRJob
 import numpy as np
 from pca import PCA
 
@@ -11,24 +11,24 @@ N = 100001
 h = 1. / N
 
 
-class MREstimatePiIntegrationCombiner(MRJob):
-  def mapper_init(self):
-    self.sum = 0
-
-  def mapper(self, _, line):
-    index = int(line)
-    x = index * h
-    if h == 0:
-      self.sum += 0.5
-    elif h != N - 1:
-      self.sum += np.sqrt(1 - x * x)
-
-  def mapper_final(self):
-    yield None, self.sum
-
-  def reducer(self, _, values):
-    yield None, h * sum(values)
-
-
-if __name__ == '__main__':
-  MREstimatePiIntegrationCombiner.run()
+# class MREstimatePiIntegrationCombiner(MRJob):
+#   def mapper_init(self):
+#     self.sum = 0
+#
+#   def mapper(self, _, line):
+#     index = int(line)
+#     x = index * h
+#     if h == 0:
+#       self.sum += 0.5
+#     elif h != N - 1:
+#       self.sum += np.sqrt(1 - x * x)
+#
+#   def mapper_final(self):
+#     yield None, self.sum
+#
+#   def reducer(self, _, values):
+#     yield None, h * sum(values)
+#
+#
+# if __name__ == '__main__':
+#   MREstimatePiIntegrationCombiner.run()
