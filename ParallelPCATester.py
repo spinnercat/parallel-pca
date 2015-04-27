3736e1cb71d846f7731fdc14cd30b2035d63c516
Marcus Schorow <mschorow@college.harvard.edu>
2015-04-27 18:00:44 -0400

    numpy

--- ParallelPCATester.py
+++ ParallelPCATester.py
@@ -1,23 +1,23 @@
 import numpy as np
 from numpy_pca import NumpyPCA
-#from parallel_pca import ParallelPCA
+from parallel_pca import ParallelPCA
 from serial_pca import SerialPCA
 import time
 
 serial_pca = SerialPCA()
-#parallel_pca = ParallelPCA()
+parallel_pca = ParallelPCA()
 np_pca = NumpyPCA()
 
 pca_calculators = [
-  {"name": "Serial", "pca": serial_pca},
+  # {"name": "Serial", "pca": serial_pca},
   # {"name": "Parallel", "pca": parallel_pca},
-  # {"name": "Numpy", "pca": np_pca}
+  {"name": "Numpy", "pca": np_pca}
 ]
 
 # Dimension of data for testing
 dimension = 3
 # N is the number of samples
-n = 4
+n = 100
 
 def generate_sample_data():
   data = []
@@ -33,8 +33,9 @@ def generate_sample_data():
 
 data = generate_sample_data()
 for calculator in pca_calculators:
+  test_data = np.copy(data)
   start_time = time.time()
-  components = calculator["pca"].do_pca(data)
+  components = calculator["pca"].do_pca(test_data)
   end_time = time.time()
   print "PCA calculation using "+calculator["name"]
   print components
--- numpy_pca.py
+++ numpy_pca.py
@@ -1,11 +1,37 @@
 # Class using numpy's PCA for comparison purposes.
 from pca import PCA
 import numpy as np
-from sklearn.decomposition import PCA
+from numpy import linalg as la
 
 
 class NumpyPCA(PCA):
   def do_pca(self, data):
-    pca = PCA(n_components=2)
-    pca.fit(data)
-    return pca.get_params()
+    data = np.transpose(data)
+    for row in data:
+      mean = float(sum(row)) / len(row)
+      for i in range(0, len(row)):
+        row[i] = row[i] - mean
+    x = data
+    print x
+
+    covariance_matrix = np.dot(data, np.transpose(data))
+    [eigenvalues,eigenvectors] =  la.eig(covariance_matrix)
+    eigenvectors = np.transpose(eigenvectors)
+    print "Eigenvalues of covariance: "
+    print eigenvalues
+    print eigenvectors
+
+    combined = []
+    for i in range(0, len(eigenvalues)):
+      combined.append((eigenvectors[i], eigenvalues[i]))
+    combined.sort(key=lambda c: c[1], reverse=True)
+
+    q = []
+    eigenvalues = []
+    for i in range(0, len(combined)):
+      q.append(combined[i][0])
+      eigenvalues.append(combined[i][1])
+    q = np.transpose(np.array(q)) # do we do this?
+
+    qq = np.dot(np.transpose(q), q)
+    y = np.dot(np.transpose(q), x)
