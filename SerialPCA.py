ccd2197b7f28467e5fb317cd82daea608ac8e387
Albert Wu <awu@college.harvard.edu>
2015-04-27 15:19:27 -0400

    change stuff a little and add papers

--- SerialPCA.py
+++ SerialPCA.py
@@ -1,2 +1,11 @@
 # Class implementing the same algorithm as we run in parallel, but in serial for comparison purposes.
 class SerialPCA(object):
+    def doPCA(self, data, blocks):
+        def center(X):
+            meanX = X.mean(axis = 1)[:,np.newaxis]
+            centeredX = X - meanX
+            return (meanX, centeredX)
+
+        (means, dataNew) = center(data)
+        for k in range(blocks):
+            cov = np.cov(dataNew[
