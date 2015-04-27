49d66eb21e4ece87b29b85a2e39d65a39ab30d6d
Albert Wu <awu@college.harvard.edu>
2015-04-27 14:57:32 -0400

    template files

---
+++ MR_pca.py
@@ -0,0 +1,28 @@
+from mrjob.job import MRJob
+import numpy as np
+
+N = 100001
+h = 1. / N
+
+class MREstimatePiIntegrationCombiner(MRJob):
+
+    def mapper_init(self):
+        self.sum = 0
+
+    def mapper(self, _, line):
+        index = int(line)
+        x = index * h
+        if h == 0:
+            self.sum += 0.5
+        elif h != N - 1:
+            self.sum += np.sqrt(1 - x * x)
+
+    def mapper_final(self):
+        yield None, self.sum
+
+    def reducer(self, _, values):
+        yield None, h * sum(values)
+
+
+if __name__ == '__main__':
+    MREstimatePiIntegrationCombiner.run()
