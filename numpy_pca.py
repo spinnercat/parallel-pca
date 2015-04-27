bdf93c4143bfae2a489fdcf5d806a1ac388242d2
Marcus Schorow <mschorow@college.harvard.edu>
2015-04-27 17:30:01 -0400

    files

--- numpy_pca.py
+++ numpy_pca.py
@@ -1,8 +1,11 @@
 # Class using numpy's PCA for comparison purposes.
 from pca import PCA
 import numpy as np
-import mdp
+from sklearn.decomposition import PCA
+
 
 class NumpyPCA(PCA):
   def do_pca(self, data):
-    return mdp.pca(data)
+    pca = PCA(n_components=2)
+    pca.fit(data)
+    return pca.get_params()
