720e2bdf0fc383bdfdf6de395b4043c6d1e02774
Albert Wu <awu@college.harvard.edu>
2015-04-29 12:33:46 -0400

    eigenmerging

--- ParallelPCATester.py
+++ ParallelPCATester.py
@@ -9,15 +9,15 @@ parallel_pca = ParallelPCA()
 np_pca = NumpyPCA()
 
 pca_calculators = [
-  # {"name": "Serial", "pca": serial_pca},
+  {"name": "Serial", "pca": serial_pca},
   # {"name": "Parallel", "pca": parallel_pca},
-  {"name": "Numpy", "pca": np_pca}
+  #{"name": "Numpy", "pca": np_pca}
 ]
 
 # Dimension of data for testing
-dimension = 6
+dimension = 3 
 # N is the number of samples
-n = 100
+n = 4
 
 np.random.seed(0)
 def generate_sample_data():
--- serial_pca.py
+++ serial_pca.py
@@ -4,8 +4,8 @@ import numpy as np
 
 class SerialPCA(object):
     def do_pca(self, data):
-        return self.doPCA_cov(data, 2)
-    #return self.doPCA_eig(data, blocks)
+        #return self.doPCA_cov(data, 2)
+        return self.doPCA_eig(data, 2)
 
     def doPCA_cov(self, data, blocks):
         def center(X):
@@ -31,20 +31,29 @@ class SerialPCA(object):
 
     def doPCA_eig(self, data, blocks):
         def center(X):
-            meanX = X.mean(axis = 1)[:,np.newaxis]
+            meanX = X.mean(axis = 0)[np.newaxis,:]
             centeredX = X - meanX
             return (meanX, centeredX)
 
         (means, dataNew) = center(data)
-        size = len(means) / blocks
+        size = len(data) / blocks
         covs = []
-        eigs = np.array([[]])
+        eigs = np.array([])
         for k in range(blocks):
-            covs.append(np.cov(dataNew[(k * blocks):((k + 1) * blocks)]))
+            dataBlock = dataNew[(k * blocks):((k + 1) * blocks),:]
+            covs.append(1./size * np.dot(dataBlock.T, dataBlock))
             w, v = np.linalg.eig(covs[k])
-            eigs = np.hstack(eigs, v)
+            if len(eigs) == 0:
+                eigs = v
+            else:
+                eigs = np.hstack((eigs, v))
 
+        print eigs.shape
         R = 1 / len(means) * np.dot(eigs.T, eigs)
         wR, vR = np.linalg.eig(R)
-        vT = np.dot(np.dot(eigs, vR), np.inv(np.sqrt(len(means) * vR)))
-        return vT
+        wR[wR < 0] = 0.1
+        print wR
+        inv_sqrt = size * np.diag(wR**(-0.5))
+        vT = np.dot(np.dot(eigs, vR), inv_sqrt)
+        return vT.T
+
