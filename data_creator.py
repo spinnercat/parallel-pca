756b3d7219f6d176597ba7f1b868d89df62a9046
Albert Wu <awu@college.harvard.edu>
2015-05-01 20:38:40 -0400

    specs for data creator

---
+++ data_creator.py
@@ -0,0 +1,8 @@
+"""
+Format for file: Each line ot the output file
+should have all the data for a block, i.e.,
+the subset of data each processing element works on. In this file,
+I want a variable with the number of dimensions of each datum
+as well as the total number of data points, so I can import those
+variables into another file.
+"""
--- parallel_pca.py
+++ parallel_pca.py
@@ -7,28 +7,55 @@ class ParallelPCA(PCA):
   def do_pca(self, data):
     raise Exception("Not implemented")
 
-N = 100001
-h = 1. / N
+#TODO(mardo): form of input?
+# dimensions
+# blocks?
+# size?
+class MRPCACovParallel(MRJob):
+    #def mapper_init(self):
+    # self.sum = 0
 
+  def mapper(self, _, line):
+    dataBlock = np.array(float(line.split()))
+    yield None, 1./size * np.dot(dataBlock.T, dataBlock)
 
-# class MREstimatePiIntegrationCombiner(MRJob):
-#   def mapper_init(self):
-#     self.sum = 0
-#
-#   def mapper(self, _, line):
-#     index = int(line)
-#     x = index * h
-#     if h == 0:
-#       self.sum += 0.5
-#     elif h != N - 1:
-#       self.sum += np.sqrt(1 - x * x)
-#
-#   def mapper_final(self):
-#     yield None, self.sum
-#
-#   def reducer(self, _, values):
-#     yield None, h * sum(values)
-#
-#
-# if __name__ == '__main__':
-#   MREstimatePiIntegrationCombiner.run()
+   #def mapper_final(self):
+   #  yield None, self.sum
+
+  def reducer(self, _, values):
+    total_cov = np.zeros(values[0].shape)
+    for cov in values:
+      total_cov += cov
+
+    w, v = np.linalg.eig(total_cov)
+    yield w, v
+
+class MRPCAEigenParallel(MRJob):
+  def mapper_init(self):
+    self.sum = 0
+
+  def mapper(self, _, line):
+    dataBlock =
+    cov = 1./size * np.dot(dataBlock.T, dataBlock)
+    w, v = np.linalg.eig(covs[k])
+
+  #def mapper_final(self):
+  #  yield None, self.sum
+
+  def reducer(self, _, values):
+    for eig in eigs:
+      eigs = np.hstack((eigs, v))
+    total_cov = np.zeros(values[0].shape)
+    for cov in values:
+      total_cov += cov
+
+    w, v = np.linalg.eig(total_cov)
+    R = 1 / len(means) * np.dot(eigs.T, eigs)
+    wR, vR = np.linalg.eig(R)
+    wR[wR < 0] = 0.1
+    inv_sqrt = size * np.diag(wR**(-0.5))
+    vT = np.dot(np.dot(eigs, vR), inv_sqrt)
+    yield wR, vT.T
+
+if __name__ == '__main__':
+    MRPCACovParallel.run()
