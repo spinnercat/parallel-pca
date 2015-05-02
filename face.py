aee37b6e04b1b753f393ae1da2940da3bc122ebc
Marcus Schorow <mschorow@college.harvard.edu>
2015-05-02 02:57:32 -0400

    new images

--- face.py
+++ face.py
@@ -9,7 +9,7 @@ height = 400
 
 for path, dirnames, filenames in os.walk('cohn-kanade'):
   for filename in filenames:
-      if filename == ".DS_Store" or filename != "S014_002_02411100.png":
+      if filename == ".DS_Store" or filename == "S014_002_02411100.png":
         continue
       image_path = os.path.join(path, filename)
       image = Image.open(image_path)
