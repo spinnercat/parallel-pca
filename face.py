59f6aa5767f08a516dee258c960b8509f9d02e55
Marcus Schorow <mschorow@college.harvard.edu>
2015-05-02 02:35:00 -0400

    new images

--- face.py
+++ face.py
@@ -1,15 +1,18 @@
 from PIL import Image
 import os
 
-width = 640
-height = 425
+width_offset = 75
+height_offset = 25
+
+width = 500
+height = 400
 
 for path, dirnames, filenames in os.walk('cohn-kanade'):
   for filename in filenames:
-      if filename == ".DS_Store":
+      if filename == ".DS_Store" or filename != "S014_002_02411100.png":
         continue
       image_path = os.path.join(path, filename)
-      print image_path
       image = Image.open(image_path)
-      image = image.crop((0,0, width, height))
-      image.show()
+      image = image.crop((width_offset,height_offset, width + width_offset, height + height_offset))
+      image = image.convert("LA")
+      image.save(image_path)
